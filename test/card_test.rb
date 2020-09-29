fd241e28b872b17a3b079676a744e03af21d2515
Ida <ida.goitom@yahoo.com>
2020-09-29 07:32:22 -0700

    exercise

--- test/card_test.rb
+++ test/card_test.rb
@@ -18,6 +18,7 @@ describe Card do
       expect {Card.new(0, :diamonds)}.must_raise ArgumentError
       expect {Card.new(14, :diamonds)}.must_raise ArgumentError
       expect {Card.new(14, :clovers)}.must_raise ArgumentError
+      expect {Card.new(1, :clovers)}.must_raise ArgumentError
     end
   end
 
@@ -26,6 +27,11 @@ describe Card do
     it "to_s returns a readable String value logically for values 2-10" do
       # Test to ensure that to_s works for cards values 2-10
       # for example:  "2 of diamonds"
+      # Act
+      card = Card.new(2, :diamonds)
+      # Assert
+      expect(card.to_s).must_equal "2 of diamonds"
+
     end
 
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
@@ -40,6 +46,18 @@ describe Card do
       #  11: Jack
       #  12: Queen
       #  13: King
+      #
+      face_cards = {
+          1 => "Ace",
+          11 => "Jack",
+          12 => "Queen",
+          13 => "King"
+      }
+      face_cards.each do |number, face_value|
+        card = Card.new(number, :hearts)
+
+        expect(card.to_s).must_equal "#{face_value} of hearts"
+      end
     end
   end
 
