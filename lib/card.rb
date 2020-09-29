b480f0c1f2ac63de5c577544c13722e79ecb750e
Ida <ida.goitom@yahoo.com>
2020-09-29 08:03:28 -0700

    tdd

--- lib/card.rb
+++ lib/card.rb
@@ -21,17 +21,28 @@ class Card
   end
 
   def to_s
-    if value == 1
-      return "Ace of #{suit.to_s}"
-    elsif value == 11
-      return "Jack of #{suit.to_s}"
-    elsif value == 12
-      return "Queen of #{suit.to_s}"
-    elsif value == 13
-      return "King of #{suit.to_s}"
-    else
-      return "#{value} of #{suit.to_s}"
+    # if value == 1
+    #   return "Ace of #{suit.to_s}"
+    # elsif value == 11
+    #   return "Jack of #{suit.to_s}"
+    # elsif value == 12
+    #   return "Queen of #{suit.to_s}"
+    # elsif value == 13
+    #   return "King of #{suit.to_s}"
+    # else
+    #   return "#{value} of #{suit.to_s}"
+    # end
+    card_text_values = {
+        1 => "Ace",
+        11 => "Jack",
+        12 => "Queen",
+        13 => "King"
+    }
+
+    if card_text_values[value].nil?
+      card_text_values[value] = value
     end
+    return "#{card_text_values[value]} of #{suit.to_s}"
   end
 
 end
